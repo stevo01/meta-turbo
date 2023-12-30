e0d593baedeefa5122ed26223f6726477de1dd0e
Steffen Volkmann <volkmann@devpool.net>
2023-12-30 09:55:52 +0100

    docker: update docker files

--- build-oe/src/run.sh
+++ build-oe/src/run.sh
@@ -9,7 +9,7 @@ function add_user() {
     echo Create user with
     echo "  user id = $CURRENT_UID"
     echo "  user name = $CURRENT_USER"
-    useradd -u $CURRENT_UID --shell /bin/bash $CURRENT_USER
+    useradd -u $CURRENT_UID -p $CURRENT_PASSWORD --shell /bin/bash $CURRENT_USER
 }
 
 
@@ -29,6 +29,8 @@ function run (){
     trap 'sighandler_TERM' 15
 
     add_user
+
+    modprobe tun
 
     echo "wait for terminate signal"
     while [  "$STOP_CONT" = "no"  ] ; do
